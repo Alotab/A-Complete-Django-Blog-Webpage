c80b914eed20867defda39b3bb50d373a92a0629
Albert Osei Frimpong <Alotab@yahoo.com>
2023-01-25 04:01:16 +0000

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Post
+from django.db.models import Count
+from taggit.models import Tag
+from pprint import pprint
+
 
 # Create your views here.
 
@@ -8,4 +12,11 @@ def post_list(request):
     trending_posts = Post.published.all().order_by('-publish')[:4]
     latest_post = Post.published.all().order_by('-publish')[:1]
     top_stories = Post.published.all().order_by('-publish')[5:9]
-    return render(request, 'blog/home.html', {'posts': posts, 'trending_posts': trending_posts, 'latest_post':latest_post, 'top_stories': top_stories})
+    # post_tags_id = posts.tags.value_list('id', flat=True)
+    # similar_posts = Post.published.filter(tags__post=posts).annotate()
+    # similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    tech_post = Post.published.filter(tag_slug=['politics'])
+    
+    return render(request, 'blog/home.html', {'posts': posts, 'trending_posts': trending_posts, 'latest_post':latest_post, 'tech_post': tech_post })
+
+
